a8838fb26b0c2368f02d849f9dd77582c6dd6970
Joaquim Serafim <joaquim.serafim@gmail.com>
2013-05-25 19:02:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1>Vagrant and Puppet configuration with Apache</h1><br>
+<br>
+Vagrant configuration with puppet to create a Virtual Box machine with Ubuntu Server 12.10 and Apache (this module also support php - check in the apache folder module))
+<br>
+<br>
+Installation<br>
+<br>
+Install Vagrant 1.2.2<br>
+Install Virtual Box 4.2.12<br>
+<br>
+<br>
+Run - vagrant up<br>
+SSH - vagrant ssh<br>
+Halt - vagrant halt<br>
+<br>
+<br>
+Puppet Manifest will install:<br>
+apache<br>
+wget<br>
+git<br>
+vim<br>
+htop<br>!
---
+++ vagrant/puppet/manifests/main.pp
@@ -0,0 +1,34 @@
+class apt_update {
+    exec { "aptGetUpdate":
+        command => "sudo apt-get update",
+        path => ["/bin", "/usr/bin"]
+    }
+}
+
+class othertools {
+    package { "git":
+        ensure => latest,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "vim-common":
+        ensure => latest,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "curl":
+        ensure => present,
+        require => Exec["aptGetUpdate"]
+    }
+
+    package { "htop":
+        ensure => present,
+        require => Exec["aptGetUpdate"]
+    }
+}
+
+#class { 'apache': }
+
+include apache
+include apt_update
+include othertools
